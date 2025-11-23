9ea97c1d0386006046c1478bc12a6633030dad88
LeenAlsahli <leen@leens-MacBook-Pro-3.local>
2025-11-23 20:15:58 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Where To Go
+
+**Where To Go** is a smart tourism web application designed to help users discover and book local events across Saudi Arabia based on their current mood ، The system supports personalized event recommendations, favorites, mood-based themes, and a simple booking process.
+
+## Features
+
+- Mood-based user interface (Light / Dark / Natural)
+- User registration and login
+- Browse and search events
+- Filter events by mood
+- Add events to favorites
+- View event details
+- Booking confirmation screen
+- Profile page with user information
+
+
+## Technologies Used
+
+- **Frontend**: Vue.js
+- **Hosting**: GitHub
+- **Tools**: Vite, Node.js, CSS
+
+## How to Run the Project Locally
+
+bash
+npm install
+npm run dev
---
+++ tests/nonfunctional.test.js
@@ -0,0 +1,59 @@
+import { describe, it, expect, vi } from 'vitest'
+import { mount } from '@vue/test-utils'
+import Browsing from '../src/components/pages/Browsing.vue'
+
+describe('Browsing Component Tests', () => {
+
+  it('should render Browsing component without crashing', () => {
+    const wrapper = mount(Browsing)
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  it('should render Event Cards', () => {
+    const wrapper = mount(Browsing)
+    expect(wrapper.findAll('.event-card').length).toBeGreaterThan(0)
+  })
+
+  it('back arrow should exist and clickable', async () => {
+    const mockRouter = { go: vi.fn() }
+    const wrapper = mount(Browsing, {
+      global: {
+        mocks: { $router: mockRouter }
+      }
+    })
+    const backArrow = wrapper.find('.back-arrow')
+    expect(backArrow.exists()).toBe(true)
+    await backArrow.trigger('click')
+    expect(mockRouter.go).toHaveBeenCalledWith(-1)
+  })
+
+  it('formatCategory returns correct titles', () => {
+    const wrapper = mount(Browsing)
+    const map = {
+      food: " Food & Dining",
+      art: " Art & Culture",
+      adventure: " Adventure",
+      poetry: " Poetry & Music"
+    }
+    Object.keys(map).forEach(key => {
+      expect(wrapper.vm.formatCategory(key)).toBe(map[key])
+    })
+  })
+
+  it('goToDetails calls router.push with correct params', () => {
+    const mockRouter = { push: vi.fn() }
+    const wrapper = mount(Browsing, {
+      global: {
+        mocks: { $router: mockRouter }
+      }
+    })
+    const testEvent = { id: 123, title: 'Test Event' }
+    wrapper.vm.goToDetails(testEvent)
+    expect(mockRouter.push).toHaveBeenCalledWith({
+      name: 'EventDetails',
+      params: { id: 123 },
+      query: { data: JSON.stringify(testEvent) }
+    })
+  })
+
+})
